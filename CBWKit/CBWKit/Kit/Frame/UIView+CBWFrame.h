c20ccf12ef7094f7d13755618b5a1bb2bcecf681
codeChenBW <861754186@qq.com>
2016-04-21 11:15:28 +0800

    分类 frame

---
+++ CBWKit/CBWKit/Kit/Frame/UIView+CBWFrame.h
@@ -0,0 +1,13 @@
+//
+//  UIView+CBWFrame.h
+//  CBWKit
+//
+//  Created by 陈博文 on 16/4/21.
+//  Copyright © 2016年 陈博文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (CBWFrame)
+
+@end
---
+++ CBWKit/CBWKit/Kit/Frame/UIView+CBWFrame.m
@@ -0,0 +1,13 @@
+//
+//  UIView+CBWFrame.m
+//  CBWKit
+//
+//  Created by 陈博文 on 16/4/21.
+//  Copyright © 2016年 陈博文. All rights reserved.
+//
+
+#import "UIView+CBWFrame.h"
+
+@implementation UIView (CBWFrame)
+
+@end
