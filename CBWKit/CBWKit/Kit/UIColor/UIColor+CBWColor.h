51911cde5f1371ce2e25f4586feeb273b9d7a029
codeChenBW <861754186@qq.com>
2016-04-22 15:58:35 +0800

    颜色分类工具

---
+++ CBWKit/CBWKit/Kit/UIColor/UIColor+CBWColor.h
@@ -0,0 +1,13 @@
+//
+//  UIColor+CBWColor.h
+//  CBWKit
+//
+//  Created by 陈博文 on 16/4/22.
+//  Copyright © 2016年 陈博文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (CBWColor)
++ (UIColor *)colorWithHexString: (NSString *) stringToConvert;
+@end
