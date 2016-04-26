488323bf040f7c8b1568e8847671cdef069b65e0
codeChenBW <861754186@qq.com>
2016-04-26 19:13:33 +0800

    payView-- 貌似不怎么好封装,以后就按照这个用吧,反正这个东西的数据源和代理都在提示框里面,如果要使用 里面的东西可以使用一个 block

---
+++ payAlertView/payAlertView/CBWPayView.h
@@ -0,0 +1,13 @@
+//
+//  CBWPayView.h
+//  payAlertView
+//
+//  Created by 陈博文 on 16/4/26.
+//  Copyright © 2016年 陈博文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBWPayView : UIView
+
+@end
---
+++ payAlertView/payAlertView/CBWPayView.m
@@ -0,0 +1,13 @@
+//
+//  CBWPayView.m
+//  payAlertView
+//
+//  Created by 陈博文 on 16/4/26.
+//  Copyright © 2016年 陈博文. All rights reserved.
+//
+
+#import "CBWPayView.h"
+
+@implementation CBWPayView
+
+@end
