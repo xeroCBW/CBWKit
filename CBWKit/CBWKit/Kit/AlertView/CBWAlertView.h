d8987f91c7a32fdd3d85bfb735799aad00f83254
codeChenBW <861754186@qq.com>
2016-04-29 18:37:08 +0800

    alertView 修改版

---
+++ CBWKit/CBWKit/Kit/AlertView/CBWAlertView.h
@@ -0,0 +1,13 @@
+//
+//  CBWAlertView.h
+//  CBWKit
+//
+//  Created by 陈博文 on 16/4/29.
+//  Copyright © 2016年 陈博文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBWAlertView : UIViewController
+
+@end
---
+++ CBWKit/CBWKit/Kit/AlertView/CBWAlertView.m
@@ -0,0 +1,13 @@
+//
+//  CBWAlertView.m
+//  CBWKit
+//
+//  Created by 陈博文 on 16/4/29.
+//  Copyright © 2016年 陈博文. All rights reserved.
+//
+
+#import "CBWAlertView.h"
+
+@implementation CBWAlertView
+
+@end
