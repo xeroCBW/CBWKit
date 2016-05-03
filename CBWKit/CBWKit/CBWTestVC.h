371f8f047493f67095116855b52e958f397034ec
codeChenBW <861754186@qq.com>
2016-05-03 15:41:38 +0800

    testView 提交

---
+++ CBWKit/CBWKit/CBWTestVC.h
@@ -0,0 +1,13 @@
+//
+//  CBWTestVC.h
+//  CBWKit
+//
+//  Created by 陈博文 on 16/4/29.
+//  Copyright © 2016年 陈博文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CBWTestVC : UIViewController
+
+@end
---
+++ CBWKit/CBWKit/CBWTestVC.m
@@ -0,0 +1,19 @@
+//
+//  CBWTestVC.m
+//  CBWKit
+//
+//  Created by 陈博文 on 16/4/29.
+//  Copyright © 2016年 陈博文. All rights reserved.
+//
+
+#import "CBWTestVC.h"
+
+@implementation CBWTestVC
+
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+}
+@end
